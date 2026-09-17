4642e5138a33c1c9267ab5864351d0f26c247c0f
santoshkeshrinirvana <95208636+santoshkeshrinirvana@users.noreply.github.com>
2026-09-17 13:44:41 +0530

    Update Google Analytics
    Update Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Google_Sans_Flex } from "next/font/google";
+import Script from "next/script";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 import "./globals.css";
@@ -27,6 +28,24 @@ export default async function RootLayout({
   ]);
   return (
     <html lang="en" data-scroll-behavior="smooth">
+        <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2QNSEVJYKB"
+          strategy="beforeInteractive"
+        />
+
+        <Script id="google-analytics" strategy="beforeInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2QNSEVJYKB');
+          `}
+        </Script>
+      </head>
       <body className={googleSansFlex.variable}>
         <Header data={headerData} />
         {children}
